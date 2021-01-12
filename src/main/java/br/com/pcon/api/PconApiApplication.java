988fd2f11d355d9d36e006b542969045749b8c9b
ana.slima6@gmail.com <ana.slima6@gmail.com>
2021-01-12 19:07:46 -0300

    Criacao do projeto PConsulting

---
+++ src/main/java/br/com/pcon/api/PconApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.pcon.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PconApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PconApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pcon/api/PconApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pcon.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PconApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
